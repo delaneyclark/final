aa42f19433f7178bf40607ebdc20ad6eddef968a
delaney clark <delaneyclark6@gmail.com>
2015-04-27 21:22:41 -0400

    added color code
    to test, not working

--- code.js
+++ code.js
@@ -2,9 +2,21 @@ $(document).ready(function(){
               $.getJSON("http://cooper-union-stars-proxy.herokuapp.com/api/v1/stars", 
                   function(response) {
            		console.log(response);
+           		
+     //       		for(var i=0; i<15; i++) {
+  							
+					// 	if ( == 1) {
+							
+					// 	} else if () {
 
+					// 	} else if () {
+
+					// 	} else {
+							
+					// 		}
+					// };
                     }    
-
+                    
                    )     
             
             
