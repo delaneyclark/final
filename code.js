d75bcee0f7964d4529cd979ed75d75aafa9c316a
delaney clark <delaneyclark6@gmail.com>
2015-04-27 21:34:40 -0400

    .log working, added notes

--- code.js
+++ code.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
               $.getJSON("http://cooper-union-stars-proxy.herokuapp.com/api/v1/stars", 
                   function(response) {
            		console.log(response);
-           		
-     //       		for(var i=0; i<15; i++) {
-  							
+
+            		for(var i=0; i<500; i++) {
+  							console.log(response[i].colorb_v);
 					// 	if ( == 1) {
 							
 					// 	} else if () {
@@ -14,7 +14,7 @@ $(document).ready(function(){
 					// 	} else {
 							
 					// 		}
-					// };
+					};
                     }    
                     
                    )     
